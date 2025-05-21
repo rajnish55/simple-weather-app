7c307fbcb66b4a0ca1d06b0129e107e3fa2ed4f5
Rajnish Raj <77603954+rajnish55@users.noreply.github.com>
2025-05-21 14:17:49 +0530

    Updated

--- weather_app.py
+++ weather_app.py
@@ -12,6 +12,7 @@ city = st.text_input("Enter City Name", key = 'city')
 def get_info(data):
     name = data['name']
     temp = data['main']['temp']
+    country = data['sys']['country']
     feels_like = data['main']['feels_like']
     humidity = data['main']['humidity']
     description = data['weather'][0]['description']
@@ -19,6 +20,7 @@ def get_info(data):
 
     st.markdown("### Current weather :")
     st.info(f"""City : {name} \n
+Country : {country} \n
 Temperature : {temp}°C ,  Feels Like : {feels_like}°C \n
 Humidity : {humidity}% \n
 Wind Speed : {wind_speed} km/h \n
